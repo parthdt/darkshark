48c2469832c4b73211c46ecac41d40ec9c419a38
GGB-21 <49618001+GGB-21@users.noreply.github.com>
2020-11-23 19:13:58 +0530

    added extension icon display

--- Browser_Extension/sniff.js
+++ Browser_Extension/sniff.js
@@ -1,1 +1,8 @@
 //sniffing password
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+    if(request.todo == "showPageAction"){
+        chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+            chrome.pageAction.show(tabs[0].id);
+        });
+    }
+});
